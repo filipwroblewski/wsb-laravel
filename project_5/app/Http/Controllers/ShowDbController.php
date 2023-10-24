21baaba574566e896771af60cd68cfca8e0cdcd4
filipwroblewski <filip.wroblewski.row@gmail.com>
2023-10-24 14:54:35 +0200

    select with where

--- project_5/app/Http/Controllers/ShowDbController.php
+++ project_5/app/Http/Controllers/ShowDbController.php
@@ -9,6 +9,9 @@ class ShowDbController extends Controller
 {
     public function showDbTable(){
         // return DB::select('select * from users_test');
-        return DB::table('users_test')->get();
+        // return DB::table('users_test')->get();
+        return DB::table('users_test')
+        ->where('lastName', 'Nowak')
+        ->get();
     }
 }
