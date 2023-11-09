169273b206ed55e36095aa88299eb26c6f425937
filipwroblewski <filip.wroblewski.row@gmail.com>
2023-11-09 14:41:24 +0100

    create fake data and add them to db

--- project_4/app/Http/Controllers/CreateFakeData.php
+++ project_4/app/Http/Controllers/CreateFakeData.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Faker\Factory as Faker;
+use Illuminate\Support\Facades\DB;
 
 class CreateFakeData extends Controller
 {
@@ -13,7 +14,6 @@ class CreateFakeData extends Controller
         for ($i = 0; $i < 5; $i++){
             $users[] = [
                 'name' => $faker->firstName,
-                'lastName' => $faker->lastName,
                 'email' => $faker->email,
                 'password' => bcrypt($faker->password),
                 'created_at' => now(),
@@ -21,6 +21,9 @@ class CreateFakeData extends Controller
             ];
         }
 
-        print_r($users[0]);
+        //DB::insert('insert into users (name, email, password, created_at, updated_at) values (?,?,?,?,?)', array_values($users[0]));
+        $res = DB::table('users')->insert($users);
+
+        print_r($res);
     }
 }
